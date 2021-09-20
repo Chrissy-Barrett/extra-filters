d116bcea5f79771ac262a7d8f695583d71317e9f
Chris Barrett <chris.barrett@defra.gov.uk>
2021-09-20 11:47:51 +0100

    Tidying up the format
    Moving the new code onto a new line and tabbing in the indent.

--- autoSetup.js
+++ autoSetup.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 fs.readFile("../../../lib/utils.js", 'utf8', function(err, data) {
   let re = new RegExp(/(var filters \= Object.assign\(coreFilters, customFilters\))/, 'gm');
-  let formatted = data.replace(re, "var extraFilters = require('../app/extra-filters.js')(env)/nvar filters = Object.assign(coreFilters, customFilters, extraFilters)");
+  let formatted = data.replace(re, "var extraFilters = require('../app/extra-filters.js')(env)\n\tvar filters = Object.assign(coreFilters, customFilters, extraFilters)");
 
   fs.writeFile("../../../lib/utils.js", formatted, 'utf8', function(err) {
     if (err) { return console.log(err);
